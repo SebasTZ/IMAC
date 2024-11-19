dbbd9cbb1bee8c91187f8d010341e0c24894be1f
SebasTZ <sebastz3120@gmail.com>
2024-11-19 18:50:16 -0500

    Cambios trabajo.php

--- app/Models/Trabajo.php
+++ app/Models/Trabajo.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Trabajo extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the pedido associated with the trabajo.
+     */
+    public function pedido()
+    {
+        return $this->belongsTo(Pedido::class);
+    }
 }
