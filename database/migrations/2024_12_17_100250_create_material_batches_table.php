f4d5ec7b6b6fd58c5b7db99c1cc414eb7f45547e
SebasTZ <sebastz3120@gmail.com>
2024-12-17 16:13:07 -0500

    Proyecto final 1

---
+++ database/migrations/2024_12_17_100250_create_material_batches_table.php
@@ -0,0 +1,30 @@
+<?php
+// filepath: /c:/laragon/www/IMAC/database/migrations/2023_01_04_235325_create_material_batches_table.php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('material_batches', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('material_id')->constrained()->onDelete('cascade');
+            $table->integer('cantidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('material_batches');
+    }
+};
